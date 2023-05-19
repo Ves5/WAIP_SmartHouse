db0f77c9830c42bf6931a07c7ddb9b7a051f8f62
Kacper Ochotny <trinerxd@gmail.com>
2023-05-19 14:31:38 +0200

    bare-bones GUI

--- src/pg/waip/smarthouse/GUI.java
+++ src/pg/waip/smarthouse/GUI.java
@@ -2,24 +2,81 @@ package pg.waip.smarthouse;
 
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
+import java.awt.event.ActionEvent;
+import javax.swing.*;
 import javax.swing.border.EmptyBorder;
-public class GUI extends JPanel {
+public class GUI extends JFrame {
 
+    private JPanel content;
     private Main mMain;
     private JLabel mText;
 
+    private JButton startBtn, stopBtn, acBtn, wmBtn;
+
     public GUI(Main main){
+        // set up frame
         mMain = main;
+        setTitle(Configuration.INSTANCE.getProperty("app.name"));
+        getContentPane().setLayout(new BorderLayout());
+        getContentPane().add(content = new JPanel(), BorderLayout.CENTER);
+        // set up content
         JPanel textPanel = new JPanel();
         textPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
-        mText = new JLabel(Configuration.INSTANCE.getName());
+        mText = new JLabel(Configuration.INSTANCE.getProperty("app.description"));
         textPanel.add(mText);
         textPanel.setBorder(new EmptyBorder(8,8,8,8));
+
+        content.setLayout(new BorderLayout());
+        content.add(textPanel, BorderLayout.CENTER);
         /* buttons */
-        setLayout(new BorderLayout());
-        add(textPanel, BorderLayout.CENTER);
+        initButtons();
 //        updateState();
+        pack();
+        setLocation(100, 100);
+//        setSize(200, 100);
+        setVisible(true);
+        updateState();
+    }
+
+    private void initButtons(){
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
+        buttonPanel.add(startBtn = new JButton(
+                new AbstractAction("Start") {
+                    public void actionPerformed(ActionEvent actionEvent) {
+                        mMain.start();
+                    }
+                }
+        ));
+        buttonPanel.add(stopBtn = new JButton(
+                new AbstractAction("Stop") {
+                    public void actionPerformed(ActionEvent actionEvent) {
+                        mMain.stop();
+                    }
+                }
+        ));
+        buttonPanel.add(acBtn = new JButton(
+                new AbstractAction("Trigger AC") {
+                    public void actionPerformed(ActionEvent actionEvent) {
+                        mMain.triggerAC();
+                    }
+                }
+        ));
+        buttonPanel.add(wmBtn = new JButton(
+                new AbstractAction("Trigger WM") {
+                    public void actionPerformed(ActionEvent actionEvent) {
+                        mMain.triggerWM();
+                    }
+                }
+        ));
+        buttonPanel.setBorder(new EmptyBorder(0, 8, 8, 8));
+        content.add(buttonPanel, BorderLayout.SOUTH);
+    }
+
+    public void updateState(){
+        startBtn.setEnabled(!mMain.isStarted());
+        stopBtn.setEnabled(mMain.isStarted());
+        acBtn.setEnabled(mMain.isStarted());
+        wmBtn.setEnabled(mMain.isStarted());
     }
 }
--- src/pg/waip/smarthouse/Main.java
+++ src/pg/waip/smarthouse/Main.java
@@ -13,11 +13,17 @@ public class Main {
     private Feature feature;
     private GUI gui;
 
+    private boolean isStarted = false;
+    public boolean isStarted(){
+        return isStarted;
+    }
+
     public static void main(String[] args) throws Exception {
         new Main();
     }
 
     public Main() throws IOException{
+        System.out.println("Starting SmartHouse application initialization");
         // loading .ini file from /resources (no clue how it knows where or which file but works)
         Configuration.INSTANCE.load(this);
         initGUI();
@@ -26,17 +32,31 @@ public class Main {
     /* This should be moved to GUI class
     * Was used for testing if it works with simulator */
     private void initGUI(){
-        JFrame f = new JFrame();
-        f.setTitle(Configuration.INSTANCE.getProperty("app.name"));
-        f.getContentPane().setLayout(new BorderLayout());
-        f.getContentPane().add(gui = new GUI(this), BorderLayout.CENTER);
-        f.pack();
-        f.setLocation(100, 100);
-        f.setVisible(true);
-        f.addWindowListener(new WindowAdapter() {
+        gui = new GUI(this);
+        gui.addWindowListener(new WindowAdapter() {
             public void windowClosing(WindowEvent e) {
+                System.out.println("Closing SmartHouse application.");
                 System.exit(0);
             }
         });
+    }
+
+    public void start(){
+        System.out.println("Starting SmartHouse service");
+        isStarted = true;
+        gui.updateState();
+    }
+    public void stop(){
+        System.out.println("Stopping SmartHouse service");
+        isStarted = false;
+        gui.updateState();
+    }
+
+    public void triggerAC(){
+        System.out.println("Triggered AC prompt");
+    }
+
+    public void triggerWM(){
+        System.out.println("Triggered WashingMachine prompt");
     }
 }
